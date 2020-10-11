30b3eab51d90ff77cf085656afb1cf95c9231d1c
luckyandrean <71387629+luckyandrean@users.noreply.github.com>
2020-10-11 10:15:22 +0700

    Add files via upload

---
+++ Back-end/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyCmHk8KO1h79e7J1ADoHOnmqz-KgU6BasQ",
+  authDomain: "scream-6e688.firebaseapp.com",
+  databaseURL: "https://scream-6e688.firebaseio.com",
+  projectId: "scream-6e688",
+  storageBucket: "scream-6e688.appspot.com",
+  messagingSenderId: "743703815066",
+  appId: "1:743703815066:web:b4500c73c73e6e56271023",
+  measurementId: "G-YGV5T50S98"
+};
